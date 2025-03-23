dc193f3a8aaf326968ea00cc336f22fd8db96841
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-03-23 13:52:52 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,44 @@
+# Operations in List
+lst = ['mango','grapes','pineapple','apple','banana']
+# Length of List
+print('length of list:',len(lst))
+# First element of List
+
+print(lst[0])
+
+# Append element to List
+
+lst.append('mango')
+print(lst)
+
+
+# Remove element from List
+
+lst.remove('banana')
+
+# Sort List
+
+lst.sort()
+print (lst)
+
+# pop element from List
+
+lst.pop()
+print(lst)
+
+# reverse List
+
+lst.reverse()
+print(lst)
+
+
+# Slice List
+
+print(lst[1:3])
+
+# Clear List
+
+lst.clear()
+print(lst)
+
+
---
+++ pos&neg.py
@@ -0,0 +1,12 @@
+#check the number is positive or negative
+
+num = int(input("enter any number:"))
+if num > 0:
+    print("your number is positive")
+elif num == 0:
+    print("your number is not positive or negetive")
+else:
+    print("your number is negetive")
+
+
+
