e5efb27205d024d279808ecc30a3829d936f17fb
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-03-28 17:54:05 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,4 @@
+# 11's table
+
+for i in range(1,51):
+    print("11 x ",i,"=",11 * i)
---
+++ while.py
@@ -0,0 +1,10 @@
+# sum of natural numbers from 1 to 1000
+
+num = 1
+sum = 0
+
+while (num <= 1000):
+    sum = sum + num
+    num = num + 1
+
+print(sum)
